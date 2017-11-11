91ba39179fd4cd5fd863808843d3b468fe4786e5
Kawewut Chujit <kawewut.chu@gmail.com>
2017-11-11 16:52:12 +0700

    checked instruction order

--- src/assembler.py
+++ src/assembler.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import sys
 
-
 def assembler_r():
     instructions = []
     in_file = open(sys.argv[2], "r")
@@ -9,7 +8,30 @@ def assembler_r():
         instructions.append(line.split("  "))
     return instructions
 
+def check_instuction(instructions):
+    for instruction in instructions:
+        instruction = instruction[1]
+        if(instruction == "add"):
+            print("add")
+        elif(instruction == "nand"):
+            print("nand")
+        elif(instruction == "lw"):
+            print("lw")
+        elif(instruction == "sw"):
+            print("sw")
+        elif(instruction == "beq"):
+            print("beq")
+        elif(instruction == "jalr"):
+            print("jalr")
+        elif(instruction == "halt"):
+            print("halt")
+        elif(instruction == "noop"):
+            print("noop")
+        else:
+            print("orther")
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        assembler_r()
+        instructions = assembler_r()
+        check_instuction(instructions)
