f682d96d1a329e7f05fb31008a6e7d0b58dbf09d
Kawewut Chujit <kawewut.chu@gmail.com>
2017-11-17 14:22:15 +0700

    created validator

--- src/jtype.py
+++ src/jtype.py
@@ -1,5 +1,10 @@
+import validator
 def jtype(instruction):
     opcode = 5
     regA = int(instruction[2].strip('\n'))
     regB = int(instruction[3].strip('\n'))
+
+    validator.registor_validator(regA)
+    validator.registor_validator(regB)
+
     return opcode << 22 + regA << 19 + regB << 16
