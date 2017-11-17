1d73f67d3e84a907571282d03f31f07c76fb48a2
Kawewut Chujit <kawewut.chu@gmail.com>
2017-11-17 20:30:59 +0700

    fixed label validator

--- src/validator.py
+++ src/validator.py
@@ -1,14 +1,18 @@
 import sys
 def registor_validator(registor):
     if(registor < 0 or registor > 7):
-        sys.exit(1)
+        sys.exit("invalid register")
 
 def offset_validator(offset):
     if(offset < -32768 or offset > 32767):
-        sys.exit(1)
+        sys.exit("invalid offset")
     
 def label_isvalid(labels, label):
-    if labels.has_key(label) or len(label) > 6:  
-        sys.exit(1)
+    if labels.has_key(label):
+        sys.exit("dupicate label")
+    elif len(label) > 6:  
+        sys.exit("exceed 6 characters label")
+    elif label[0] >= "0" and label[0] <= "9":
+        sys.exit("1st character is number")
     else:
         return True
