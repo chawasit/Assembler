59bea948d9434d83a54ef5e76a7dd612e5ab3b2a
drum2007 <beam555@hotmail.com>
2017-11-18 23:32:42 +0700

    fix comment

--- src/itype.py
+++ src/itype.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python2
 import validator
-def i_type(instruction,labels,index):                                       # initial opcode and register for each instruction
+def i_type(instruction,labels,index):
+
+    # initial opcode and register for each instruction                                       
     if instruction[1] == 'lw':                                                     
         op = 2
     elif instruction[1] == 'sw':
         op = 3
     elif instruction[1] == 'beq':
         op = 4
-    if int(instruction[2].strip('\n')) > 7:                                 # throw exception
+
+    # throw exception    
+    if int(instruction[2].strip('\n')) > 7:                                 
         raise ValueError("Arrays must have the same size")
     else:
         rs = int(instruction[2].strip('\n'))
     rt = int(instruction[3].strip('\n'))
-    try:                                                                    # check if offset value is integer
+    try:
+
+        # check if offset value is integer                                                                    
         offset = int(instruction[4].strip('\n'))
-    except:                                                                 # offset value is not interger
+    except:
+
+        # offset value is not interger                                                                 
         offset = labels[instruction[4].strip('\n')]
         if instruction[1] == 'beq':
             offset -= index + 1
-            
-    validator.registor_validator(rs)                                        # check if register is valid
+
+    # check if register is valid        
+    validator.registor_validator(rs)                                        
     validator.registor_validator(rt)                                        
     validator.offset_validator(offset)
 
-    return (op << 22) + (rs << 19) + (rt << 16) + (offset & 0xffff)         # shift value to 32-bit
+    # shift value to 32-bit
+    return (op << 22) + (rs << 19) + (rt << 16) + (offset & 0xffff)        
--- src/jtype.py
+++ src/jtype.py
@@ -1,10 +1,14 @@
 import validator
-def jtype(instruction):                                                  # initial opcode and register for each instruction
+def jtype(instruction):
+
+    # initial opcode and register for each instruction                                                  
     opcode = 5
     regA = int(instruction[2].strip('\r\n'))
     regB = int(instruction[3].strip('\r\n'))
 
-    validator.registor_validator(regA)                                  # check if register is valid
+    # check if register is valid
+    validator.registor_validator(regA)                                  
     validator.registor_validator(regB)
 
-    return (opcode << 22) + (regA << 19) + (regB << 16)                 # shift value to 32-bit
+    # shift value to 32-bit
+    return (opcode << 22) + (regA << 19) + (regB << 16)                 
